0f9062a80df6744a1adfa65022571e22000e6f79
PedroTrudes <pedro.jtrudes@gmail.com>
2025-12-30 09:27:57 -0300

    :lipstick: feat: creating global styles for the application.

---
+++ src/app/components/card-car/card-car.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-car',
+  imports: [],
+  templateUrl: './card-car.component.html',
+  styleUrl: './card-car.component.css'
+})
+export class CardCarComponent {
+
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LoginLayoutComponent } from '../../components/login-layout/login-layout.component';
+import { CardCarComponent } from '../../components/card-car/card-car.component';
 
 @Component({
   selector: 'app-login',
-  imports: [LoginLayoutComponent],
+  imports: [LoginLayoutComponent, CardCarComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
